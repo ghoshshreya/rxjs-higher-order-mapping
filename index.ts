bd429018ec60888e639c537661595bc058e31f5d
ghoshshreya <shrghosh08@gmail.com>
2023-08-03 12:16:48 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,13 +1,37 @@
-import { concatMap, mergeMap, switchMap, exhaustMap, from, of } from 'rxjs';
+import {
+  concatMap,
+  mergeMap,
+  switchMap,
+  exhaustMap,
+  from,
+  of,
+  interval,
+  take,
+  throwError,
+} from 'rxjs';
 import { delay } from 'rxjs/operators';
 
 var emitValues = (operator) => {
   from([1, 2, 3, 4, 5])
-    .pipe(operator((x) => of(x).pipe(delay(500))))
-    .subscribe((x) => console.log(x));
+    .pipe(
+      operator((x) => {
+        // Simulating an error scenario
+        // if (x === 4) {
+        //   return of(x).pipe(delay(500), () =>
+        //     throwError(() => new Error('Inner Observale error'))
+        //   );
+        // }
+        return of(x).pipe(delay(500));
+      })
+    )
+    .subscribe({
+      next: (x) => console.log(x),
+      error: (error) => console.error('Oops', error),
+      complete: () => console.log('Completed'),
+    });
 };
 
-emitValues(concatMap);
+// emitValues(concatMap);
 emitValues(mergeMap);
-emitValues(switchMap);
-emitValues(exhaustMap);
+// emitValues(switchMap);
+// emitValues(exhaustMap);
